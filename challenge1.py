128d7f06e87b628ffa462baa73e2ec3650671d69
GuilleLopezDM <147752999+GuilleLopezDM@users.noreply.github.com>
2026-02-18 15:14:19 -0300

    Add files via upload
    challenge concluido

--- challenge1.py
+++ challenge1.py
@@ -1,129 +1,212 @@
+#Se importa las librerias random y math
 import random
+import math
+
+#se crea la clase tablero
 class Tablero:
-    def __init__(self,filas,columnas):
+    vacio="*"
+    def __init__(self,filas,columnas): #metodo constructor
         self.filas = filas
         self.columnas = columnas
-        self.tablero=[['*' for i in range(columnas)] for j in range(filas)] #crea una matriz de 0's con el tamaño del tablero
-class Raton:
+        self.tablero=[[self.vacio for i in range(columnas)] for j in range(filas)]
+    def dentro(self,x,y):
+        return 0<=x<self.columnas and 0<=y<self.filas
+    def caminable(self,x,y): 
+        return  self.dentro(x,y) and self.tablero[y][x]==self.vacio
+    
+
+class Animal:
+    def __init__(self,tablero,posicion_x,posicion_y):
+         self.tablero=tablero
+         self.posicion_x=posicion_x
+         self.posicion_y=posicion_y
+    def mover_animal(self,dx,dy):
+        nx,ny=self.posicion_x+dx,self.posicion_y+dy
+        if (dx==0 and dy==0):
+            return False
+        elif self.tablero.caminable(nx,ny):
+            self.posicion_x,self.posicion_y=nx,ny
+            return True
+        else:
+            return False
+
+
+class Raton(Animal):
+    simbolo="R"
     def __init__(self,tablero):
-        self.posicion_x = 0
-        self.posicion_y = 0
-        self.tablero = tablero
+        super().__init__(tablero,1,random.randint(0,tablero.filas-1))
 
-    def mover_raton(self):
-        print("Ingrese el movimiento del raton: ")
-        while True:
-                movimiento = input(
-                "1-Para mover a la izquierda\n"
-                "2-Para mover a la derecha\n"
-                "3-Para mover arriba\n"
-                "4-Para mover abajo\n"
-                "5-Para salir\n"
-                )
 
-                if movimiento == '1':
-                    if self.posicion_x > 0:
-                        self.tablero[self.posicion_y][self.posicion_x] = '*'  # borra la posición anterior del ratón
-                        self.posicion_x -= 1
-                        self.tablero[self.posicion_y][self.posicion_x] = 'R'  # coloca el ratón en la nueva posición
-                        break
-                    else:
-                        print("No puedes mover a la izquierda, estás en el borde del tablero.")
-
-                elif movimiento == '2':
-                    if self.posicion_x < len(self.tablero[0]) - 1:
-                        self.tablero[self.posicion_y][self.posicion_x] = '*'  # borra la posición anterior del ratón
-                        self.posicion_x += 1
-                        self.tablero[self.posicion_y][self.posicion_x] = 'R'  # coloca el ratón en la nueva posición
-                        break
-                    else:
-                        print("No puedes mover a la derecha, estás en el borde del tablero.")
-
-                elif movimiento == '3':
-                    if self.posicion_y > 0:
-                        self.tablero[self.posicion_y][self.posicion_x] = '*'  # borra la posición anterior del ratón
-                        self.posicion_y -= 1
-                        self.tablero[self.posicion_y][self.posicion_x] = 'R'  # coloca el ratón en la nueva posición
-                        break
-                    else:
-                        print("No puedes mover hacia arriba, estás en el borde del tablero.")
-
-                elif movimiento == '4':
-                    if self.posicion_y < len(self.tablero) - 1:
-                        self.tablero[self.posicion_y][self.posicion_x] = '*'  # borra la posición anterior del ratón
-                        self.posicion_y += 1
-                        self.tablero[self.posicion_y][self.posicion_x] = 'R'  # coloca el ratón en la nueva posición
-                        break
-                    else:
-                        print("No puedes mover hacia abajo, estás en el borde del tablero.")
-
-                elif movimiento == '5':
-                    print("Saliendo del juego.")
-                    return False
-
-                else:
-                    print("Movimiento inválido.")
-class Gato:
+class Gato(Animal):
+    simbolo="G"
     def __init__(self,tablero):
-        self.posicion_x = len(tablero[0]) - 1
-        self.posicion_y = len(tablero) - 1
-        self.tablero = tablero
-    def mover_gato(self):
-        while True:
-            movimiento=random.randint(1,4)
-            if movimiento == 1 and self.posicion_x > 0:
-                self.tablero[self.posicion_y][self.posicion_x] = '*'  # borra la posición anterior del gato
-                self.posicion_x -= 1
-                self.tablero[self.posicion_y][self.posicion_x] = 'G'  # coloca el gato en la nueva posición
-                break
-            elif movimiento == 2 and self.posicion_x < len(self.tablero[0]) - 1:
-                self.tablero[self.posicion_y][self.posicion_x] = '*'  # borra la posición anterior del gato
-                self.posicion_x += 1
-                self.tablero[self.posicion_y][self.posicion_x] = 'G'  # coloca el gato en la nueva posición
-                break
-            elif movimiento == 3 and self.posicion_y > 0:
-                self.tablero[self.posicion_y][self.posicion_x] = '*'  # borra la posición anterior del gato
-                self.posicion_y -= 1
-                self.tablero[self.posicion_y][self.posicion_x] = 'G'  # coloca el gato en la nueva posición
-                break
-            elif movimiento == 4 and self.posicion_y < len(self.tablero) - 1:
-                self.tablero[self.posicion_y][self.posicion_x] = '*'  # borra la posición anterior del gato
-                self.posicion_y += 1
-                self.tablero[self.posicion_y][self.posicion_x] = 'G'  # coloca el gato en la nueva posición
-                break
+        super().__init__(tablero,tablero.columnas-1,random.randint(0,tablero.filas-1))
+
+class Estado:
+    def __init__(self,rx,ry,gx,gy,rondas):
+        self.rx=rx
+        self.ry=ry
+        self.gx=gx
+        self.gy=gy
+        self.rondas=rondas
+    def copia(self):
+        return Estado(self.rx,self.ry,self.gx,self.gy,self.rondas)
+    def termina_estado(self):
+        if (self.rx==self.gx and self.ry==self.gy):
+            return "GATO"
+        elif self.rondas>=20:
+            return "RATON"
+        else:
+            return None
 class Juego:
-    def __init__(self,gato,raton,tablero):
-        self.gato=gato
-        self.raton=raton
+    movs=[(1,0),(0,1),(-1,0),(0,-1)]
+    def __init__(self,tablero,raton,gato):
         self.tablero=tablero
-        self.tablero[self.raton.posicion_y][self.raton.posicion_x]='R'
-        self.tablero[self.gato.posicion_y][self.gato.posicion_x]='G'
+        self.raton=raton
+        self.gato=gato
 
+    def heuristica(self,estado):
+        manhattan=abs(estado.rx - estado.gx) + abs(estado.ry - estado.gy)
+        return manhattan
+    
     def mostrar_juego(self):
-        for fila in self.tablero:
+        aux=[fila.copy() for fila in self.tablero.tablero]
+        aux[self.raton.posicion_y][self.raton.posicion_x]=self.raton.simbolo
+        aux[self.gato.posicion_y][self.gato.posicion_x]=self.gato.simbolo
+        for fila in aux:
             print(' '.join(fila))
+        print("\n")
+
+    def movimientos_validos(self,x,y):
+        validos=[]
+        for (dx,dy) in self.movs:
+            nx,ny=x+dx,y+dy
+            if self.tablero.caminable(nx,ny):
+                validos.append((dx,dy))
+        return validos
+    
+        
+    def estado_actual(self,ronda_actual):
+        rx=self.raton.posicion_x
+        ry=self.raton.posicion_y
+        gx=self.gato.posicion_x
+        gy=self.gato.posicion_y
+        return Estado(rx,ry,gx,gy,ronda_actual)
+    def siguiente_estado(self,estado,turno,mov):
+        dx=mov[0]
+        dy=mov[1]
+        nuevo_estado=estado.copia()
+        if turno=="R":
+            nuevo_estado.rx=estado.rx+dx
+            nuevo_estado.ry=estado.ry+dy
+        elif turno=="G":
+            nuevo_estado.gx=estado.gx+dx
+            nuevo_estado.gy=estado.gy+dy
+            nuevo_estado.rondas=estado.rondas+1
+        return nuevo_estado
+    def evaluar(self,estado):
+        resultado=estado.termina_estado()
+        if resultado!=None:
+            if resultado=="RATON":
+                return +100000
+            elif resultado=="GATO":
+                return -100000
+        return self.heuristica(estado)
+    def minimax(self,estado,depth,turno,alfa,beta):
+        if depth==0:
+            return self.evaluar(estado)
+        if estado.termina_estado()!=None:
+            return self.evaluar(estado)
+        elif turno=="R":
+            mejor=-math.inf
+            movs=self.movimientos_validos(estado.rx,estado.ry)
+            if movs==[]:
+                return self.evaluar(estado)
+            for movimiento in movs:
+                hijo=self.siguiente_estado(estado,"R",movimiento)
+                score=self.minimax(hijo,depth-1,"G",alfa,beta)
+                mejor=max(mejor,score)
+                alfa=max(alfa,mejor)
+                if beta<=alfa:
+                    break
+            return mejor
+        elif turno=="G":
+            mejor=math.inf
+            movs=self.movimientos_validos(estado.gx,estado.gy)
+            if movs==[]:
+                return self.evaluar(estado)
+            for movimiento in movs:
+                hijo=self.siguiente_estado(estado,"G",movimiento)
+                score=self.minimax(hijo,depth-1,"R",alfa,beta)
+                mejor=min(mejor,score)
+                beta=min(beta,mejor)
+                if beta<=alfa:
+                    break
+            return mejor
+    def mejor_movimiento(self,estado,turno,depth):
+        alfa,beta=-math.inf,math.inf
+        if turno=="R":
+            movs=self.movimientos_validos(estado.rx,estado.ry)
+            if movs==[]:
+                return None
+            mejor_puntaje=-math.inf
+            mejor_mov=movs[0]
+            for movimiento in movs:
+                hijo=self.siguiente_estado(estado,"R",movimiento)
+                score=self.minimax(hijo,depth-1,"G",alfa,beta)
+                if score>mejor_puntaje:
+                    mejor_puntaje=score
+                    mejor_mov=movimiento
+                alfa=max(alfa,mejor_puntaje)
+                if beta<=alfa:
+                    break
+            return mejor_mov
+        elif turno=="G":
+            movs=self.movimientos_validos(estado.gx,estado.gy)
+            if movs==[]:
+                return None
+            mejor_puntaje=+math.inf
+            mejor_mov=movs[0]
+            for movimiento in movs:
+                hijo=self.siguiente_estado(estado,"G",movimiento)
+                score=self.minimax(hijo,depth-1,"R",alfa,beta)
+                if score<mejor_puntaje:
+                    mejor_puntaje=score
+                    mejor_mov=movimiento
+                beta=min(beta,mejor_puntaje)
+                if beta<=alfa:
+                    break
+            return mejor_mov
+
     def ejecutar_juego(self):
         self.mostrar_juego()
+        cant_rondas=0
+        depth=4
         while True:
-            if self.raton.mover_raton() is False:
-                print("El juego ha terminado por el jugador.")
+            estado_raton=self.estado_actual(cant_rondas)
+            mov=self.mejor_movimiento(estado_raton,"R",depth)
+            if mov!=None:
+                self.raton.mover_animal(mov[0],mov[1])
+            estado_raton=self.estado_actual(cant_rondas)
+            resultado=estado_raton.termina_estado()
+            if resultado!=None:
+                print(f"El ganador es el: {resultado}")
                 break
-            if self.raton.posicion_x == self.gato.posicion_x and self.raton.posicion_y == self.gato.posicion_y:
-                print("¡El gato ha atrapado al ratón! Fin del juego.")
-                self.mostrar_juego()
-                break
-            self.gato.mover_gato()
-            if self.raton.posicion_x == self.gato.posicion_x and self.raton.posicion_y == self.gato.posicion_y:
-                print("¡El gato ha atrapado al ratón! Fin del juego.")
-                self.mostrar_juego()
+            estado_gato=self.estado_actual(cant_rondas)
+            mov=self.mejor_movimiento(estado_gato,"G",depth)
+            if mov!=None:
+                self.gato.mover_animal(mov[0],mov[1])
+            cant_rondas=cant_rondas+1
+            estado_gato=self.estado_actual(cant_rondas)
+            resultado=estado_gato.termina_estado()
+            if resultado!=None:
+                print(f"El ganador es el: {resultado}")
                 break
             self.mostrar_juego()
 
-
-filas = int(input("Ingrese el número de filas del tablero: "))
-columnas = int(input("Ingrese el número de columnas del tablero: "))
-tablero1 = Tablero(filas, columnas)
-raton1=Raton(tablero1.tablero)
-gato1=Gato(tablero1.tablero)
-juego1=Juego(gato1,raton1,tablero1.tablero)
-juego1.ejecutar_juego()
+if __name__ == "__main__":
+    tablero1 = Tablero(6, 6)
+    raton1 = Raton(tablero1)
+    gato1 = Gato(tablero1)
+    juego1 = Juego(tablero1, raton1, gato1)
+    juego1.ejecutar_juego()
